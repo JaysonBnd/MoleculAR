6f0b5658e888e0bf09a99f77e168a9ae05b5b7d7
umbrachorum <quentin1.vedrenne@epitech.eu>
2024-01-18 13:31:09 +0100

    fix: api

--- api_molecular/api.py
+++ api_molecular/api.py
@@ -9,14 +9,14 @@ api = Api(app)
 @app.route('/api/molecular/<name>/<mole>', methods=['GET'])
 def get_user(name, mole):
     pwd = os.getcwd()
-    dirs = os.listdir('./molecules/')
+    dirs = os.listdir('./mol/')
     file = None
     for dir in dirs:
         if name == dir:
-            files = os.listdir(pwd + '/molecules/' + name)
+            files = os.listdir(pwd + '/mol/' + name)
             for file in files:
                 if file.endswith(".cjson") and os.path.splitext(file)[0] == mole:
-                    with open(pwd + '/molecules/' + name + '/' + file) as f:
+                    with open(pwd + '/mol/' + name + '/' + file) as f:
                         data = json.load(f)
                         return jsonify(data)
     
