dd647fc6d393a81902b948b3aa4542a1b182d239
umbrachorum <quentin1.vedrenne@epitech.eu>
2024-01-18 12:12:02 +0100

    add: api for get melocular values

---
+++ api_molecular/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+import os
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route('/api/molecular/<name>/<mole>', methods=['GET'])
+def get_user(name, mole):
+    pwd = os.getcwd()
+    dirs = os.listdir('./molecules/')
+    file = None
+    for dir in dirs:
+        if name == dir:
+            files = os.listdir(pwd + '/molecules/' + name)
+            for file in files:
+                if file.endswith(".cjson") and os.path.splitext(file)[0] == mole:
+                    with open(pwd + '/molecules/' + name + '/' + file) as f:
+                        data = json.load(f)
+                        return jsonify(data)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
